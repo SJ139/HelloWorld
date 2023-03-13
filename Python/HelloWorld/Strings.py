ceecd12324a3a2f87d4072bf89ac22c7f505b506
SJ139 <90908149+SJ139@users.noreply.github.com>
2023-03-13 18:32:27 -0400

    Python Hello World...Videos 25

--- Python/HelloWorld/Strings.py
+++ Python/HelloWorld/Strings.py
@@ -5,4 +5,12 @@ print('We can even include "quotes" in Strings')
 print("hello" + " world")
 greeting = "Hello"
 name = input("Please enter your name:")
+
 print(greeting + ' ' + name)
+age = 24
+print(age)
+
+age = "2 Years"
+print(age)
+print(type(greeting))
+print(type(age))
---
+++ Python/HelloWorld/escapechar.py
@@ -0,0 +1,14 @@
+splitString = "This String has been\nsplit over \nseveral\nlines"
+print(splitString)
+
+tabbedString = "\t1 \t2 \t3 \t4 \t5"
+print (tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+#Or
+print("The pet shop owner said\"No, no, 'e's uh,...he's resting\".")
+#or
+print("""The pet shop owner said "No, no, 'e's uh,...he's resting".""")
+
+print("c:\\users\\sjohnston\\notes.txt")
+print(r"c:\users\sjohnston\notes.txt")
