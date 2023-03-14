3cc10517cd6af7b64e9a88f6215b0c2b2847b8b8
SJ139 <90908149+SJ139@users.noreply.github.com>
2023-03-14 07:41:22 -0400

    Python Hello World...Videos 33

---
+++ Python/HelloWorld/sliceback.py
@@ -0,0 +1,17 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+backwards = letters[25::-1]
+print(backwards)
+print()
+qpo = letters[16:13:-1]
+print(qpo)
+print()
+edcba = letters[4::-1]
+print(edcba)
+print()
+last8 = letters[25:17:-1]
+print(last8)
+last82 = letters[:-9:-1]
+print(last82)
+
+last4 = letters[-4:]
+print(last4)
--- Python/HelloWorld/string2.py
+++ Python/HelloWorld/string2.py
@@ -2,4 +2,5 @@ parrot = "Norwegian Blue"
 print(parrot[0:6]) #Norweg
 print(parrot[3:5]) #we
 
-print(parrot[:])
+print(parrot[:6] + parrot[6:])
+print()
